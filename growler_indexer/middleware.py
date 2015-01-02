4d2b3d6dc76b8c301f18673c52c3ed8690a1d696
Andrew Kubera <andrewkubera@gmail.com>
2015-01-02 14:01:15 -0500

    Now sends files

--- growler_indexer/middleware.py
+++ growler_indexer/middleware.py
@@ -21,13 +21,22 @@ class Indexer():
 
   def __call__(self, req, res):
     tpath = path.join(self.abs, *req.path[1:].split('/'))
+    print (req.protocol)
+    print (req.headers, req.headers['host'])
     try:
-      ul = "<ul><li>%s</li></ul>" % ("</li><li>".join(listdir(tpath)))
+      filenames = listdir(tpath)
     except FileNotFoundError:
       head = self.head_tmpl.substitute(title="404 Not Found")
       body = "<h1>404 - Not Found</h1><p>The requested URL %s was not found on this server." % (self.abs)
-      res.send_html(self.html_tmpl.substitute(head=head, body=body))
+      res.send_html(self.html_tmpl.substitute(head=head, body=body), 404)
       return
+    except Exception as e:
+      print ("ERRRR", e,tpath)
+      res.send_file(tpath)
+      return
+
+    filelinks = ["<a href='{0}'>{1}</a>".format('http://' + req.headers['host'] + req.path + '/' + f, f) for f in filenames]    
+    ul = "<ul><li>%s</li></ul>" % ("</li><li>".join(filelinks))
 
     title = "Index of %s" % (self.path)
     body = "<h1>Index of %s</h1>%s" % (self.abs, ul)
