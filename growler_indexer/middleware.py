3b4218bb121940d25122a17d17f87bd563cab991
Andrew Kubera <andrewkubera@gmail.com>
2015-01-02 12:16:07 -0500

    Middleware responds with the absolute path of specified directory

--- growler_indexer/middleware.py
+++ growler_indexer/middleware.py
@@ -2,7 +2,15 @@
 # growler_indexer/middleware.py
 #
 
+from os import (path)
+
 class Indexer():
   
-  def __init__(self, dir):
-    pass
+  def __init__(self, dir, prefix = "/"):
+    print("[Indexer::__init__]")
+    self.path = dir
+    self.abs = path.abspath(dir)
+    print("  hosting", self.abs)
+
+  def __call__(self, req, res):
+    res.send_html("<h1>%s</h1>" % (self.abs))
