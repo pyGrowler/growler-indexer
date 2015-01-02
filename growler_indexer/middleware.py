705873f6bcf43685bda1b1d41973de15ad352aea
Andrew Kubera <andrewkubera@gmail.com>
2015-01-02 13:00:01 -0500

    Returns 404 on directories which do not exist.

--- growler_indexer/middleware.py
+++ growler_indexer/middleware.py
@@ -2,15 +2,34 @@
 # growler_indexer/middleware.py
 #
 
-from os import (path)
+from os import (path, listdir)
+from string import (Template)
+
 
 class Indexer():
+  
+  html_tmpl = Template("""<!DOCTYPE html><html><head>$head</head><body>$body</body></html>""")
+  head_tmpl = Template("""<title>$title</title>""")
   
   def __init__(self, dir, prefix = "/"):
     print("[Indexer::__init__]")
     self.path = dir
-    self.abs = path.abspath(dir)
+    print("dir:",dir)
+    print (path.expanduser(dir))
+    self.abs = path.abspath(path.expanduser(dir))
     print("  hosting", self.abs)
 
   def __call__(self, req, res):
-    res.send_html("<h1>%s</h1>" % (self.abs))
+    tpath = path.join(self.abs, *req.path[1:].split('/'))
+    try:
+      ul = "<ul><li>%s</li></ul>" % ("</li><li>".join(listdir(tpath)))
+    except FileNotFoundError:
+      head = self.head_tmpl.substitute(title="404 Not Found")
+      body = "<h1>404 - Not Found</h1><p>The requested URL %s was not found on this server." % (self.abs)
+      res.send_html(self.html_tmpl.substitute(head=head, body=body))
+      return
+
+    title = "Index of %s" % (self.path)
+    body = "<h1>Index of %s</h1>%s" % (self.abs, ul)
+    head = self.head_tmpl.substitute(title=title)
+    res.send_html(self.html_tmpl.substitute(head=head, body=body))
