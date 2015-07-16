33557ceca9b623d172f8ff1644f98dcfa6a401b4
Andrew Kubera <andrewkubera@gmail.com>
2015-07-16 00:37:12 -0400

    Version bump to 0.3.0 - compatability with growler 0.5

--- growler/indexer/__init__.py
+++ growler/indexer/__init__.py
@@ -2,7 +2,8 @@
 # growler/indexer/__index__.py
 #
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
+__date__ = 'Jul 16, 2015'
 __author__ = "Andrew Kubera"
 __contact__ = "andrew.kubera@gmail.com"
 __license__ = "Apache v2.0"
